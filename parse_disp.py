f8de3b4a80c83849bc890ccfff0833871f44a169
rkn2 <rkn2@princeton.edu>
2018-06-08 13:30:31 -0400

    updates

--- parse_disp.py
+++ parse_disp.py
@@ -4,22 +4,27 @@ This script takes the output of 3DEC for displacement and converts it into somet
 
 import glob
 import numpy as np
+#paths = ['C:/Users/Rebecca Napolitano/Documents/datafiles/zippervaults/eq_disp/', 
+#         'C:/Users/Rebecca Napolitano/Documents/datafiles/zippervaults/2018_2_13_eq/']
 
-#path = 'C:/Users/Rebecca Napolitano/Documents/datafiles/Romanbondingcourses/2017_11_15_lowfric_persistent/nobc/'    
-def parse_disp(path):
-    stress_files = glob.glob(path + '*_disp.dat')
+def parse_disp(paths):
     
-    filenames = [f.replace('_disp.dat', '') for f in stress_files]
-    
-    for filename in filenames:
-        fid = open(filename+'_disp.dat', 'r+') #open disp file from 3dec
-        txt = fid.read()
-        fid.close()
-        txt = txt.replace('(','').replace(')','').replace(',',' ').replace('\n 0\n',' ') #remove extra characters
-        dat = np.fromstring(txt,sep=' \n')[:-1] # ask numpy to convert string to array
-        all_dat = np.reshape(dat[:],(-1,4)) # reshape array from 1D to 2D #-1 means "dont care how long it is, just make 4 columns"
-        np.savetxt(filename+'_disp_py.dat', all_dat)
+    for path in paths:
+        disp_files = glob.glob(path + '*_disp.dat')
+        
+        filenames = [f.replace('_disp.dat', '') for f in disp_files]
+        print(len(filenames))
+        
+        for filename in filenames:
+            print(filename)
+            fid = open(filename+'_disp.dat', 'r+') #open disp file from 3dec
+            txt = fid.read()
+            fid.close()
+            txt = txt.replace('(','').replace(')','').replace(',',' ').replace('\n 0\n',' ') #remove extra characters
+            dat = np.fromstring(txt,sep=' \n')[:-1] # ask numpy to convert string to array
+            all_dat = np.reshape(dat[:],(-1,4)) # reshape array from 1D to 2D #-1 means "dont care how long it is, just make 4 columns"
+            np.savetxt(filename+'_disp_py.dat', all_dat)
   
 
   
-#parse_stress(path)
+#parse_disp(paths)
